fd55e4b8a9be22cc695966e6d8f494a9ce72f23e
Frank-Tao <zhiweitao.suzhou@gmail.com>
2019-12-28 17:48:52 +0800

    refactor-规范化代码

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,11 +1,9 @@
-import static org.fest.util.Arrays.format;
-
 public class Rover {
 
-    public final static String EAST = "E";
-    public final static String WEST = "W";
-    public final static String SOUTH = "S";
-    public final static String NORTH = "N";
+    public static final String EAST = "E";
+    public static final String WEST = "W";
+    public static final String SOUTH = "S";
+    public static final String NORTH = "N";
 
 
     private Area area;
@@ -62,19 +60,18 @@ public class Rover {
         if (this.direction.equals(Rover.EAST)) {
             int newPositionX = this.positionX + 1 > this.area.getMaxX() ? this.area.getMaxX() : this.positionX + 1;
             this.positionX = newPositionX;
-            return;
+
         } else if (this.direction.equals(Rover.WEST)) {
             int newPositionX = this.positionX - 1 < 0 ? 0 : this.area.getMaxX();
             this.positionX = newPositionX;
-            return;
+
         } else if (this.direction.equals(Rover.SOUTH)) {
             int newPositionY = this.positionY - 1 < 0 ? 0 : this.positionY - 1;
             this.positionY = newPositionY;
-            return;
+
         } else if (this.direction.equals(Rover.NORTH)) {
             int newPositionY = this.positionY + 1 > this.area.getMaxY() ? this.area.getMaxY() : this.positionY + 1;
             this.positionY = newPositionY;
-            return;
         }
         return;
     }
@@ -82,16 +79,16 @@ public class Rover {
     public void turnLeft() {
         if (this.direction.equals(Rover.EAST)) {
             this.direction = Rover.NORTH;
-            return;
+
         } else if (this.direction.equals(Rover.WEST)) {
             this.direction = Rover.SOUTH;
-            return;
+
         } else if (this.direction.equals(Rover.SOUTH)) {
             this.direction = Rover.EAST;
-            return;
+
         } else if (this.direction.equals(Rover.NORTH)) {
             this.direction = Rover.WEST;
-            return;
+
         }
         return;
     }
@@ -99,16 +96,16 @@ public class Rover {
     public void turnRight() {
         if (this.direction.equals(Rover.EAST)) {
             this.direction = Rover.SOUTH;
-            return;
+
         } else if (this.direction.equals(Rover.WEST)) {
             this.direction = Rover.NORTH;
-            return;
+
         } else if (this.direction.equals(Rover.SOUTH)) {
             this.direction = Rover.WEST;
-            return;
+
         } else if (this.direction.equals(Rover.NORTH)) {
             this.direction = Rover.EAST;
-            return;
+
         }
         return;
     }
