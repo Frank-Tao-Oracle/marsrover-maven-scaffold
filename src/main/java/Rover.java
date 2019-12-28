e48e41ede41be6a9ec216430d6e28cdc38a02faf
Frank-Tao <zhiweitao.suzhou@gmail.com>
2019-12-28 16:10:21 +0800

    pass

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -2,6 +2,13 @@ import static org.fest.util.Arrays.format;
 
 public class Rover {
 
+    public final static String EAST = "E";
+    public final static String WEST = "W";
+    public final static String SOUTH = "S";
+    public final static String NORTH = "N";
+
+
+
     private Area area;
     private int positionX;
     private int positionY;
@@ -19,5 +26,26 @@ public class Rover {
     public String getPosition() {
         String result = "" + this.positionX + this.positionY + this.direction;
         return result;
+    }
+
+    public void move() {
+        if (this.direction.equals(Rover.EAST)){
+            int newPositionX = this.positionX + 1 > this.area.getMaxX()?this.area.getMaxX():this.positionX + 1;
+            this.positionX = newPositionX;
+            return;
+        } else if (this.direction.equals(Rover.WEST)){
+            int newPositionX = this.positionX - 1 < 0?0:this.area.getMaxX();
+            this.positionX = newPositionX;
+            return;
+        } else if (this.direction.equals(Rover.SOUTH)){
+            int newPositionY = this.positionY - 1 < 0?0:this.positionY-1;
+            this.positionY = newPositionY;
+            return;
+        } else if (this.direction.equals(Rover.NORTH)){
+            int newPositionY = this.positionY + 1 > this.area.getMaxY()?this.area.getMaxY():this.positionY+1;
+            this.positionY = newPositionY;
+            return;
+        }
+        return;
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -8,8 +8,19 @@ public class MarsRoverTest {
     public void should_land_rover_on_area(){
         Area area = new Area(10, 10);
         Rover rover = new Rover();
-        rover.land(area, 5, 5, "E");
+        rover.land(area, 5, 5, Rover.EAST);
         String position = rover.getPosition();
         assertThat(position).isEqualTo("55E");
+    }
+
+    @Test
+    public void should_move_forward_when_direction_to_east(){
+        Area area = new Area(10, 10);
+        Rover rover = new Rover();
+        rover.land(area, 5, 5, Rover.EAST);
+        rover.move();
+        String position = rover.getPosition();
+        assertThat(position).isEqualTo("65E");
+
     }
 }
